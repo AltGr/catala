5afff2fb4580c8502e4fa8d9afe991ac25daddb8
Louis Gesbert <louis.gesbert@ocamlpro.com>
2023-04-15 17:07:24 +0200

    Playing with the lazy interp output

--- compiler/shared_ast/print.ml
+++ compiler/shared_ast/print.ml
@@ -474,7 +474,7 @@ let rec expr_aux :
          (fun fmt e -> lhs exprc fmt e))
       es punctuation ")"
   | EArray es ->
-    Format.fprintf fmt "@[<hv 2>%a %a@] %a" punctuation "["
+    Format.fprintf fmt "@[<hov 2>%a %a@] %a" punctuation "["
       (Format.pp_print_list
          ~pp_sep:(fun fmt () -> Format.fprintf fmt ";@ ")
          (fun fmt e -> lhs exprc fmt e))
