f0b06b3e29a00d53e009adb42bfb8dbd2fa22698
Denis Merigoux <denis.merigoux@gmail.com>
2023-04-17 18:00:30 +0200

    Coded message format adapted to editors

--- compiler/catala_utils/cli.ml
+++ compiler/catala_utils/cli.ml
@@ -94,6 +94,10 @@ let optimize_flag = ref false
 let disable_counterexamples = ref false
 let avoid_exceptions_flag = ref false
 
+type message_format_enum = Human | EditorParsable
+
+let message_format_flag = ref Human
+
 open Cmdliner
 
 let file =
@@ -117,6 +121,20 @@ let color =
         ~doc:
           "Allow output of colored and styled text. If set to $(i,auto), \
            enabled when the standard output is to a terminal.")
+
+let message_format_opt = Arg.enum ["human", Human; "editor", EditorParsable]
+
+let message_format =
+  Arg.(
+    value
+    & opt ~vopt:Human message_format_opt Human
+    & info ["message_format"]
+        ~doc:
+          "Selects the format of error and warning messages emitted by the \
+           compiler. If set to $(i,human), the messages will be nicely \
+           displayed and meant to be read by a human. If set to $(i, editor), \
+           the messages will be rendered in a way that is easily parsable by \
+           IDEs.")
 
 let unstyled =
   Arg.(
@@ -247,6 +265,7 @@ let output =
 type options = {
   debug : bool;
   color : when_enum;
+  message_format : message_format_enum;
   wrap_weaved_output : bool;
   avoid_exceptions : bool;
   backend : string;
@@ -267,6 +286,7 @@ let options =
   let make
       debug
       color
+      message_format
       unstyled
       wrap_weaved_output
       avoid_exceptions
@@ -285,6 +305,7 @@ let options =
     {
       debug;
       color = (if unstyled then Never else color);
+      message_format;
       wrap_weaved_output;
       avoid_exceptions;
       backend;
@@ -305,6 +326,7 @@ let options =
     const make
     $ debug
     $ color
+    $ message_format
     $ unstyled
     $ wrap_weaved_output
     $ avoid_exceptions
@@ -337,7 +359,8 @@ let set_option_globals options : unit =
   trace_flag := options.trace;
   optimize_flag := options.optimize;
   disable_counterexamples := options.disable_counterexamples;
-  avoid_exceptions_flag := options.avoid_exceptions
+  avoid_exceptions_flag := options.avoid_exceptions;
+  message_format_flag := options.message_format
 
 let version = "0.8.0"
 
