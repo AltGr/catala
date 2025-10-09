bce36f47828cec4ac52abc9d065e375df2fa417a
Louis Gesbert <louis.gesbert@inria.fr>
2025-10-09 18:43:54 +0200

    listc

--- stdlib/c/list_internal.c
+++ stdlib/c/list_internal.c
@@ -6,33 +6,58 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <catala_runtime.h>
-
 
 const CATALA_ARRAY(CATALA_INT) ListInternal__sequence
     (CATALA_INT begin, CATALA_INT end)
 {
-  static const catala_code_position pos[1] =
-    {{"stdlib/list_internal.catala_en", 4, 13, 4, 21}};
-  catala_error(catala_impossible, pos, 1);
-  abort();
+  mpz_t zlen;
+  size_t i, len;
+  catala_array* ret = catala_malloc(sizeof(catala_array));
+  mpz_init (zlen);
+  mpz_sub (zlen, end, begin);
+  len = mpz_sgn (zlen) > 0 ? mpz_get_ui (zlen) : 0;
+  mpz_clear (zlen);
+  printf ("%ld..%ld (%lu)\n", mpz_get_si(begin), mpz_get_si(end), len);
+  ret->size = len;
+  ret->elements = catala_malloc(len * sizeof(CATALA_INT));
+  for (i = 0; i < len; i++) {
+    mpz_t x;
+    mpz_init (x);
+    mpz_add_ui (x, begin, i);
+    ret->elements[i] = x;
+    printf ("     %02lu -- %ld\n", i, mpz_get_si (x));
+  }
+  return ret;
 }
 
 const CATALA_OPTION(void * /* any t */) ListInternal__nth_element
     (const CATALA_ARRAY(void * /* any t */) lst, CATALA_INT index)
 {
-  static const catala_code_position pos[1] =
-    {{"stdlib/list_internal.catala_en", 9, 13, 9, 24}};
-  catala_error(catala_impossible, pos, 1);
-  abort();
+  int n = mpz_get_si (index) - 1;
+  if (0 <= n && n < lst->size)
+    return catala_some(lst->elements[n]);
+  else
+    return CATALA_NONE;
 }
 
 const CATALA_ARRAY(void * /* any t */) ListInternal__remove_nth_element
     (const CATALA_ARRAY(void * /* any t */) lst, CATALA_INT index)
 {
-  static const catala_code_position pos[1] =
-    {{"stdlib/list_internal.catala_en", 14, 13, 14, 31}};
-  catala_error(catala_impossible, pos, 1);
-  abort();
+  int n = mpz_get_si (index) - 1;
+  if (0 <= n && n < lst->size) {
+    int i;
+    size_t len = lst->size;
+    catala_array* ret = catala_malloc(sizeof(catala_array));
+    ret->size = len - 1;
+    ret->elements = catala_malloc (ret->size * sizeof(void*));
+    for (i = 0; i < n; i++)
+      ret->elements[i] = lst->elements[i];
+    for (i = n+1; i < len; i++)
+      ret->elements[i-1] = lst->elements[i];
+    return ret;
+  } else {
+    return lst;
+  }
 }
-
