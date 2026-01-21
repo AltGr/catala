3c0c7561798962bbee2679925b1b5393e617c788
Louis Gesbert <louis.gesbert@inria.fr>
2026-01-21 09:55:51 +0100

    tymodules

--- runtimes/ocaml/catala_runtime.ml
+++ runtimes/ocaml/catala_runtime.ml
@@ -252,63 +252,184 @@ let duration_to_years_months_days (d : duration) : int * int * int =
 
 (* -- Runtime types and embedding -- *)
 
-type runtype =
-  | Unit
-  | Bool
-  | Money
-  | Integer
-  | Decimal
-  | Date
-  | Duration
-  | Enum of {
+type 'a runtype =
+  | Unit : unit runtype
+  | Bool : bool runtype
+  | Money : money runtype
+  | Integer : integer runtype
+  | Decimal : decimal runtype
+  | Date : date runtype
+  | Duration : duration runtype
+  | Enum : {
       name: string;
-      constr: Obj.t -> string * runvalue option;
-      constant_constructors: string list;
-      variable_constructors: (string * runtype) list
-    }
-  | Struct of {
+      constr: 'a -> string * runvalue option;
+      destr: string * runvalue option -> 'a;
+    } -> 'a runtype
+  | Struct : {
       name: string;
-      fields: (string * runtype) list
-    }
-  | External of string
-  | Array of runtype
-  | Tuple of runtype list
-  | Position
-  | Function of runtype list * runtype
+      fields: 'a -> (string * runvalue) list;
+    } -> 'a runtype
+  | External : {
+      name: string;
+      equal: code_location -> 'a -> 'a -> bool;
+      compare: code_location -> 'a -> 'a -> int;
+    } -> 'a runtype
+  | Array: 'a runtype -> 'a array runtype
+  | Tuple: ('a -> runvalue list) -> 'a runtype
+  | Position : code_location runtype
+  | Function : (('args -> 'ret) -> 'args -> runvalue) -> ('args -> 'ret) runtype (* ?? *)
 
-type runvalue = { t: runtype; v: Obj.t }
+and runvalue = RValue: { t: 'a runtype; v: 'a } -> runvalue
 
-let embed t v = { t; v = Obj.repr v }
+let embed t v = RValue { t; v }
 
-let unembed (type a) (RValue (t, v)): a runtype * a =
-  get_runtype (TAny t), Obj.magic v
+let unembed (type a) (RValue { t; v }): a runtype * a =
+  Obj.magic t, Obj.magic v
 
-let get_runtype : type a. any_runtype -> a runtype =
-  let open struct external cast : _ runtype -> a runtype = "%identity" end in
-  function TAny t -> cast t
-
-let unembed (type a) (RValue (t, v)): a runtype * a =
-  get_runtype (TAny t), Obj.magic v
+(* let get_runtype : type a. any_runtype -> a runtype =
+ *   let open struct external cast : _ runtype -> a runtype = "%identity" end in
+ *   function TAny t -> cast t
+ * 
+ * let unembed (type a) (RValue (t, v)): a runtype * a =
+ *   get_runtype (TAny t), Obj.magic v *)
 
 (* Catala types utils *)
 
 module type CatalaType = sig
   type t
-  val equal: t -> t -> bool
-  val compare: t -> t -> int
+  val equal: code_location -> t -> t -> bool
+  val compare: code_location -> t -> t -> int
   val rtype: t runtype
 end
-(*
-module Unit : CatalaType with type t = unit
-module Bool : CatalaType with type t = bool
-module Money : CatalaType with type t = money
-module Integer : CatalaType with type t = integer
-module Decimal : CatalaType with type t = decimal
-module Date : CatalaType with type t = date
-module Duration : CatalaType with type t = duration
-module List : (T: CatalaType) -> CatalaType with type t = T.t array
-module Optional : (T: CatalaType) -> CatalaType with type t = T.t array
-*)
+
+module Unit : CatalaType with type t = unit = struct
+  type t = unit
+  let equal _ _ _ = true
+  let compare _ _ _ = 0
+  let rtype = Unit
+end
+module Bool : CatalaType with type t = bool = struct
+  type t = bool
+  let equal _ = Bool.equal
+  let compare _ = Bool.compare
+  let rtype = Bool
+end
+module Money : CatalaType with type t = money = struct
+  type t = money
+  let equal _ = Z.equal
+  let compare _ = Z.compare
+  let rtype = Money
+end
+module Integer : CatalaType with type t = integer = struct
+  type t = integer
+  let equal _ = Z.equal
+  let compare _ = Z.compare
+  let rtype = Integer
+end
+module Decimal : CatalaType with type t = decimal = struct
+  type t = decimal
+  let equal _ = Q.equal
+  let compare _ = Q.compare
+  let rtype = Decimal
+end
+module Date : CatalaType with type t = date = struct
+  type t = date
+  let equal _ d1 d2 = Dates_calc.compare_dates d1 d2 = 0
+  let compare _ = Dates_calc.compare_dates
+  let rtype = Date
+end
+module Duration : CatalaType with type t = duration = struct
+  type t = duration
+
+  (* TODO: add this compare built-in to dates_calc ? *)
+  let compare pos p1 p2 =
+    let y1, m1, d1 = Dates_calc.period_to_ymds p1 in
+    let y2, m2, d2 = Dates_calc.period_to_ymds p2 in
+    match y1, y2, m1, m2, d1, d2 with
+    | _, _, _, _, 0, 0 -> Int.compare ((12 * y1) + m1) ((12 * y2) + m2)
+    | 0, 0, 0, 0, d1, d2 -> Int.compare d1 d2
+    | _ -> error UncomparableDurations [pos]
+
+  let equal pos p1 p2 =
+    Dates_calc.period_to_ymds p1 = Dates_calc.period_to_ymds p2
+    || compare pos p1 p2 = 0
+
+  let rtype = Duration
+end
+
+module Array (X: CatalaType) : CatalaType with type t = X.t array = struct
+  type t = X.t array
+  let equal pos a b =
+    Array.length a = Array.length b && Array.for_all2 (X.equal pos) a b
+
+  let compare pos a b =
+    let rec aux i =
+      if i >= Array.length a then
+        if i >= Array.length b then 0
+        else -1
+      else if i >= Array.length b then 1
+      else match X.compare pos a.(i) b.(i) with
+        | 0 -> aux (i+1)
+        | n -> n
+    in
+    aux 0
+
+  let rtype = Array X.rtype
+end
+
+module Foo : CatalaType = struct
+  type t = { foo: integer; bar: date }
+
+  let equal pos a b = Integer.equal pos a.foo b.foo && Date.equal pos a.bar b.bar
+
+  let compare pos a b = match Integer.compare pos a.foo b.foo with
+    | 0 -> Date.compare pos a.bar b.bar
+    | n -> n
+
+  let rtype = Struct {
+      name = "Foo";
+      fields = fun t -> [
+          "foo", embed Integer.rtype t.foo;
+          "bar", embed Date.rtype t.bar;
+        ]
+    }
+end
+
+module Bar : CatalaType = struct
+  type t = Foo of integer | Bar of date | Baz of (bool * integer)
+
+  let equal pos a b = match a, b with
+    | Foo a, Foo b -> Integer.equal pos a b
+    | Bar a, Bar b -> Date.equal pos a b
+    | Baz (a1, a2), Baz (b1, b2) -> Bool.equal pos a1 b1 && Integer.equal pos a2 b2
+    | _ -> false
+
+  let compare pos a b = match a, b with
+    | Foo a, Foo b -> Integer.compare pos a b
+    | Foo _, _ -> 1
+    | _, Foo _ -> -1
+    | Bar a, Bar b -> Date.compare pos a b
+    | Bar _, _ -> 1
+    | _, Bar _ -> -1
+    | Baz (a1, a2), Baz (b1, b2) -> match Bool.compare pos a1 b1 with 0 -> Integer.compare pos a2 b2 | n -> n
+
+  let rtype = Enum {
+      name = "Bar";
+      constr = fun t -> match t with
+        | Foo x -> "Foo", Some (embed Integer.rtype x)
+        | Bar x -> "Bar", Some (embed Date.rtype x)
+        | Baz x -> "Baz", Some (embed (Tuple (fun (x1, x2) -> [embed Bool.rtype x1; embed Integer.rtype x2])) x);
+    }
+end
+
+
+(* module List : (T: CatalaType) -> CatalaType with type t = T.t array = struct
+ *   type t = T.t array
+ * end
+ * module Optional : (T: CatalaType) -> CatalaType with type t = T.t array = struct
+ *   type t = T.t array
+ * end *)
+
 (* -- *)
 
 type information = string list
@@ -316,7 +437,7 @@ type information = string list
 type raw_event =
   | BeginCall of information
   | EndCall of information
-  | VariableDefinition of information * io_log * runtime_value
+  | VariableDefinition of information * io_log * runvalue
   | DecisionTaken of code_location
 
 type event =
@@ -332,7 +453,7 @@ and var_def = {
   pos : code_location option;
   name : information;
   io : io_log;
-  value : runtime_value;
+  value : runvalue;
   fun_calls : fun_call list option;
 }
 
@@ -396,8 +517,8 @@ module BufferedJson = struct
 
   (* Note: the output format is made for transition with what Yojson gave us,
      but we could change it to something nicer (e.g. objects for structures) *)
-  let rec runtime_value (type a) buf (v: a runtype * a) =
-    match v with
+  let rec runtime_value buf (RValue rv) =
+    match rv.t, rv.v with
     | Unit, () -> Buffer.add_string buf "{}"
     | Bool, b -> Buffer.add_string buf (string_of_bool b)
     | Money, m -> Buffer.add_string buf (money_to_string m)
@@ -406,37 +527,22 @@ module BufferedJson = struct
     | Date, d -> quote buf (date_to_string d)
     | Duration, d -> quote buf (duration_to_string d)
     | Enum en, e ->
-      let o = Obj.repr e in
-      let tag = Obj.tag o in
-      let constr, value =
-        if tag = Obj.int_tag then
-          List.nth en.constant_constructors (Obj.obj o : int), None
-        else
-        let constr, vty =
-          List.nth en.variable_constructors
-            (tag - Obj.first_non_constant_constructor_tag)
-        in
-        constr, Some (fun buf -> runtime_value buf (get_runtype vty, Obj.field o 0))
-      in
+      let constr, value = en.constr e in
       Printf.bprintf buf
         {|{"kind": "enum", "name": "%s", "constructor": "%s"%a}|}
         en.name constr
         (fun buf -> function
            | None -> ()
-           | Some f -> Printf.bprintf buf {|, "value": %t|} f)
+           | Some v -> Printf.bprintf buf {|, "value": %a|} runtime_value v)
         value
     | Struct str, s ->
-      let o = Obj.repr s in
-      let fields =
-        Seq.map2 (fun (fld, vty) v -> fld, (vty, v))
-          (List.to_seq str.fields)
-          (Array.to_seq (Obj.obj o))
-      in
+      let fields = str.fields s in
       Printf.bprintf buf {|{"kind": "struct", "name": "%s", "fields": {%a}}|}
         str.name
-        (list (fun buf (cstr, vty) ->
-             Printf.bprintf buf {|"%s": %a|} cstr runtime_value v))
-        str.fields
+        (fun buf ->
+           List.iter (fun (name, v) ->
+               Printf.bprintf buf {|"%a": %a|} quote name runtime_value v))
+        fields
     | (Array elts | Tuple elts) as v ->
       Printf.bprintf buf {|{"kind": %s, "value":[%a]}|}
         (match v with
@@ -881,19 +987,6 @@ let handle_exceptions (exceptions : ('a * code_location) Optional.t array) :
       (List.map
          (function Optional.Present (_, pos) -> pos | _ -> assert false)
          res)
-
-(* TODO: add this compare built-in to dates_calc ? *)
-let compare_periods pos (p1 : duration) (p2 : duration) : int =
-  let y1, m1, d1 = Dates_calc.period_to_ymds p1 in
-  let y2, m2, d2 = Dates_calc.period_to_ymds p2 in
-  match y1, y2, m1, m2, d1, d2 with
-  | _, _, _, _, 0, 0 -> Int.compare ((12 * y1) + m1) ((12 * y2) + m2)
-  | 0, 0, 0, 0, d1, d2 -> Int.compare d1 d2
-  | _ -> error UncomparableDurations [pos]
-
-let equal_periods pos (p1 : duration) (p2 : duration) : bool =
-  Dates_calc.period_to_ymds p1 = Dates_calc.period_to_ymds p2
-  || compare_periods pos p1 p2 = 0
 
 module Oper = struct
   let o_not = Stdlib.not
